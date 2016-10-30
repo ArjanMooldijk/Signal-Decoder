44a9d7bd3fd91d7a3c52ee1ba6fe2f2b3b14039e
Arjan Mooldijk <arj_moo@hotmail.com>
2016-10-30 17:12:24 +0100

    More bugs fixed
    Zwerg decoder now updated to new version

--- DCC_Schweizer_Zwergsignale_Decoder_Rev3.ino
+++ DCC_Schweizer_Zwergsignale_Decoder_Rev3.ino
@@ -1,7 +1,5 @@
-#include <Arduino.h>
-
 // Sketch for Swiss dwarfsignals
-// Version 1.1 (19-10-16)  Arjan Mooldijk
+// Version 2.0 (30-10-16)  Arjan Mooldijk
 // Thanks to Franz-Peter "MicroBahner" (Stummis) for the DCC examples
 // Thanks to Alex Leone for the TLC library
 
@@ -14,25 +12,39 @@
 #define ZwergAdr1nF   3 // Zwerg addr 1,      no Fade 3 leds !!STARTPIN!!
 #define ZwergAdr2nF   4 // Zwerg addr 2,      no Fade 3 leds
 
-// Constants:
-const unsigned int signalAdr[] =   {551, 552, 553, 554, 555, 556, 557, 558, 559, 560 };  // DCC-Signal Addresses
-const byte signalType[]        =   {ZwergAdr1F,ZwergAdr2F,ZwergAdr1F,ZwergAdr2F,ZwergAdr1F,ZwergAdr2F,ZwergAdr1F,ZwergAdr2F,ZwergAdr1F,ZwergAdr2F};  // Signal typen per adres
-const byte signalChannel[]     =   { 0, 0, 3, 3, 6, 6, 9, 9, 12, 12 };  // Eerste pin van het sein
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// AAN TE PASSEN CONSTANTEN ///////////////////////////////////////////////////////////////////////////////////////////
+// Decoder nummer. Dit wordt in de decoder zelf opgeslagen en is met de monitor uit te lezen.                       ///
+// Dan weet je welke het is.                                                                                        ///
+const byte decoderNbr = 1;                                                                                          ///
+                                                                                                                    ///
+// DCC adressen:                                                                                                    ///
+const unsigned int signalAdr[] =   {551, 552, 553, 554, 555, 556, 557, 558, 559, 560};                              ///
+                                                                                                                    ///
+// Sein types per hierboven opgegeven adres                                                                         ///
+const byte signalType[]        =   { ZwergAdr1F,ZwergAdr2F,ZwergAdr1F,ZwergAdr2F,ZwergAdr1F,
+                                     ZwergAdr2F,ZwergAdr1F,ZwergAdr2F,ZwergAdr1F,ZwergAdr2F};                       ///
+                                                                                                                    ///
+// Lichtsterkte van de leds per sein in volgorde van de opgegeven types                                             ///
 const int dimConst [16] = {10,10,13,
                            10,10,15,
                            10,10,15,
                            10,10,15,
                            10,10,15,
-                           0};      // Brightness per led (max 16) Every row is one signal
-const int anZahl = sizeof(signalType);
-const long fadeConst = 150;                // Fade in/decrease in mSec.
-const long darkDelay = 350;                // Dark phase duration in mSec.
-const long interval = 800;                 // interval to blink in mSec.
-
-
+                           0};             // Brightness per led (max 16) Every row is one signal              ///
+                                                                                                                    ///
+                                                                                                                    ///
+const int anZahl = sizeof(signalType);                                                                              ///
+const long fadeConst = 150;                 // Fade in/decrease in mSec.                                            ///
+const long darkDelay = 350;                 // Dark phase duration in mSec.                                         ///
+const long interval = 800;                  // interval to blink in mSec.                                           ///
+///////// HIERONDER HOEFT NIETS MEER AANGEPAST TE WORDEN (TENZIJ JE HET PROGRAMMA WILT VERANDEREN ;-) /////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Variables:
 NmraDcc Dcc;
+byte signalLeds[16];                        // # leds per signal
+byte signalChannel[16];                     // Eerste pin per het sein
+byte pinCounter = 0;                        // Variable to address the right pin on the TLC5940
 uint32_t startMillis;                       // millisecond variable for fade
 uint32_t endMillis;                         // millisecond variable for fade
 unsigned int LastAddr = 0;                  // last command address
@@ -51,28 +63,42 @@ void setup() {
     Tlc.update();                           // And write to Tlc5940
     Serial.begin(9600);
     while (!Serial) {}                      // wait for serial port to connect
-    Serial.println("Zwerggignal decoder nr1");
+    Serial.print("Zwerggignal decoder nr ");
+    Serial.println(decoderNbr);
     Serial.print("Adressen: ");
     Serial.println(anZahl);
     lastSeq = millis();
-
+    
     for (byte x = 0; x < anZahl; x ++) {   // set all default Halt and register starting pin per signal
       switch (signalType[x]) {
         case ZwergAdr1F:
+          signalLeds[x] = 3;
+          signalLeds[x+1] = 3;
+          signalChannel[x] = pinCounter;
+          signalChannel[x+1] = pinCounter;
+          pinCounter = pinCounter + 3;
           Serial.print("Pin: ");
           Serial.print(signalChannel[x]);
           Serial.print(" Adres: ");
           Serial.print(signalAdr[x]);
-          Serial.println(" Zwergsignal mit Fade");
-          setZwergsignalF(signalChannel[x],0);
+          Serial.print(" Zwergsignal Fade ");
+          Serial.print(signalLeds[x]);
+          Serial.println(" Leds");
+          setZwergsignalF(x,0);
           break;
         case ZwergAdr1nF:
-          Serial.print("Pin: ");
+          signalLeds[x] = 3;
+          signalLeds[x+1] = 3;
+          signalChannel[x] = pinCounter;
+          signalChannel[x+1] = pinCounter;
+          pinCounter = pinCounter + 3;Serial.print("Pin: ");
           Serial.print(signalChannel[x]);
           Serial.print(" Adres: ");
           Serial.print(signalAdr[x]);
-          Serial.println(" Zwergsignal ohne Fade");
-          setZwergsignalnF(signalChannel[x],0);
+          Serial.print(" Zwergsignal noFade ");
+          Serial.print(signalLeds[x]);
+          Serial.println(" Leds");
+          setZwergsignalnF(x,0);
           break;
         default:
           break;
@@ -96,56 +122,65 @@ void notifyDccAccState( uint16_t Addr, uint16_t BoardAddr, uint8_t OutputAddr, u
     if (Addr == signalAdr[index]) {                     // ist eigene Adresse
       if ((Addr != LastAddr) || ( LastTime < millis() - 200)) {
         switch (signalType[index]) {
-          case ZwergAdr1F:                                       //Zwergsignal Addr 1 Fahrt & Halt Fade
+//Zwergsignal Addr 1 Fahrt & Halt Fade
+          case ZwergAdr1F:
             startMillis = millis() + 1;
-            dunkelZwergsignal(signalChannel[index]);
+            dunkelZwergsignal(index);
             if ( OutputAddr & 0x1 ) {
-              setZwergsignalF(signalChannel[index], 1);
+              setZwergsignalF(index, 1);
               lastBild[signalChannel[index]] = 1;
               break;
             }
             else {
-              setZwergsignalF(signalChannel[index], 0);
+              setZwergsignalF(index, 0);
               lastBild[signalChannel[index]] = 0;
               break;
             }
-          case ZwergAdr2F:                                       //Zwergsignal Addr 2 Fahrt mit Vorsicht Fade
+
+//Zwergsignal Addr 2 Fahrt mit Vorsicht Fade
+          case ZwergAdr2F:
             startMillis = millis() + 1;
-            dunkelZwergsignal(signalChannel[index]);
+            dunkelZwergsignal(index);
             if ( OutputAddr & 0x1 ) {
-              setZwergsignalF(signalChannel[index], 2);
+              setZwergsignalF(index, 2);
               lastBild[signalChannel[index]] = 2;
               break;
             }
             else {
               if (lastBild[signalChannel[index]] == 0) {
-                setZwergsignalF(signalChannel[index], 3);
+                setZwergsignalF(index, 3);
                 lastBild[signalChannel[index]] = 3;
                 break;
               }
               else {
                 if (lastBild[signalChannel[index]] == 2) {
-                  setZwergsignalF(signalChannel[index], 1);
+                  setZwergsignalF(index, 1);
                   lastBild[signalChannel[index]] = 1;
                   break;
                 }
                 else {
-                  setZwergsignalF(signalChannel[index], 1);
+                  setZwergsignalF(index, 1);
                   break;
                 }
               }
             }
-          case ZwergAdr1nF:                                       //Zwergsignal Addr 1 Fahrt & Halt noFade
+
+//Zwergsignal Addr 1 Fahrt & Halt noFade
+          case ZwergAdr1nF:
             if ( OutputAddr & 0x1 ) {
-              setZwergsignalnF(signalChannel[index], 1);
+              setZwergsignalnF(index, 2);
+              delay(500);
+              setZwergsignalnF(index, 1);
               break;
             }
             else {
-              setZwergsignalnF(signalChannel[index], 0);
+              setZwergsignalnF(index, 0);
               break;
             }
-          case ZwergAdr2nF:                                       //Zwergsignal Addr 2 Fahrt mit Vorsicht noFade
-            setZwergsignalnF(signalChannel[index], 2);
+
+//Zwergsignal Addr 2 Fahrt mit Vorsicht noFade
+          case ZwergAdr2nF:
+            setZwergsignalnF(index, 2);
             break;
           default:
             break;
@@ -153,7 +188,7 @@ void notifyDccAccState( uint16_t Addr, uint16_t BoardAddr, uint8_t OutputAddr, u
         LastAddr = Addr;
         LastTime = millis();
         runSequence = false;
-      }
+      } 
     }
   }
 }
@@ -164,7 +199,7 @@ void setNextZwergBild() {
       startMillis = millis() + 1;
       dunkelZwergsignal(0);
       setZwergsignalF(0, 2);
-      lastBild[0] = 2;
+      lastBild[0] = 2;    
     }
     else {
       if (lastBild[0] == 2 ){
@@ -188,80 +223,65 @@ void setNextZwergBild() {
 
 
 //////////////////////////////// Zwergsignal Fade ///////////////////////////////////////////////////////////////////////
-void dunkelZwergsignal(byte startPin) {
+void dunkelZwergsignal(byte pntr) {
   endMillis = startMillis + fadeConst;
   for (byte x=0; x<3; x++){
-    if (Tlc.get(startPin+x) > 0) {
-      tlc_addFade(startPin+x, dimConst[startPin+x], 0, startMillis, endMillis);                 //If not off then fade out
-    }
+    if (Tlc.get(signalChannel[pntr]+x) > 0) tlc_addFade(signalChannel[pntr]+x, dimConst[signalChannel[pntr]+x], 0, startMillis, endMillis);                 //If not off then fade out
   }
   startMillis = endMillis + darkDelay;
 }
 //////////////
-void setZwergsignalF(byte startPin, byte Fb){
+void setZwergsignalF(byte pntr, byte Fb){
 //ch1 links unter
 //ch2 rechts unter
 //ch3 oben
  switch (Fb){
     case 0: // zet ch1 en ch2 aan en ch3 uit
        endMillis = startMillis + fadeConst * 2;
-       tlc_addFade(startPin+0, 0, dimConst[startPin+0], startMillis, endMillis);
-       tlc_addFade(startPin+1, 0, dimConst[startPin+1], startMillis, endMillis);
+       tlc_addFade(signalChannel[pntr]+0, 0, dimConst[signalChannel[pntr]+0], startMillis, endMillis);
+       tlc_addFade(signalChannel[pntr]+1, 0, dimConst[signalChannel[pntr]+1], startMillis, endMillis);
        break;
     case 1: // zet ch1 en ch3 aan, ch2 uit
        endMillis = startMillis + fadeConst * 2;
-       tlc_addFade(startPin+0, 0, dimConst[startPin+0], startMillis, endMillis);
-       tlc_addFade(startPin+2, 0, dimConst[startPin+2], startMillis, endMillis);
+       tlc_addFade(signalChannel[pntr]+0, 0, dimConst[signalChannel[pntr]+0], startMillis, endMillis);
+       tlc_addFade(signalChannel[pntr]+2, 0, dimConst[signalChannel[pntr]+2], startMillis, endMillis);
        break;
     case 2: // zet ch1 uit en ch2, ch3 aan
        endMillis = startMillis + fadeConst * 2;
-       tlc_addFade(startPin+1, 0, dimConst[startPin+1], startMillis, endMillis);
-       tlc_addFade(startPin+2, 0, dimConst[startPin+2], startMillis, endMillis);
-       break;
-    case 3: // van Fb0  via Fb2 naar Fb1
-       endMillis = startMillis + fadeConst * 2;
-       tlc_addFade(startPin+1, 0, dimConst[startPin+1], startMillis, endMillis);
-       tlc_addFade(startPin+2, 0, dimConst[startPin+2], startMillis, endMillis);
-       startMillis = endMillis + darkDelay;
-       endMillis = startMillis + fadeConst;
-       tlc_addFade(startPin+1, dimConst[startPin+1], 0, startMillis, endMillis);
-       tlc_addFade(startPin+2, dimConst[startPin+2], 0, startMillis, endMillis);
-       startMillis = endMillis + darkDelay;
-       endMillis = startMillis + fadeConst * 2;
-       tlc_addFade(startPin+0, 0, dimConst[startPin+0], startMillis, endMillis);
-       tlc_addFade(startPin+2, 0, dimConst[startPin+2], startMillis, endMillis);
+       tlc_addFade(signalChannel[pntr]+1, 0, dimConst[signalChannel[pntr]+1], startMillis, endMillis);
+       tlc_addFade(signalChannel[pntr]+2, 0, dimConst[signalChannel[pntr]+2], startMillis, endMillis);
        break;
     default:
        break;
        }
 }
-//////////////////////////////////Zwergsignal no Fade //////////////////////////////////////////////////////////
-void setZwergsignalnF(byte startPin, byte Fb){
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+void setZwergsignalnF(byte pntr, byte Fb){
 //ch1 links unter
 //ch2 rechts unter
 //ch3 oben
  switch (Fb){
     case 0: // zet ch1 en ch2 aan en ch3 uit
 //       Serial.println(" Halt");
-       Tlc.set(startPin+0, dimConst[startPin]);
-       Tlc.set(startPin+1, dimConst[startPin+1]);
-       Tlc.set(startPin+2, 0);
+       Tlc.set(signalChannel[pntr]+0, dimConst[signalChannel[pntr]]);
+       Tlc.set(signalChannel[pntr]+1, dimConst[signalChannel[pntr]+1]);
+       Tlc.set(signalChannel[pntr]+2, 0);
        break;
     case 1: // zet ch1 en ch3 aan, ch2 uit
 //       Serial.println(" Fahrt");
-       Tlc.set(startPin+0, dimConst[startPin]);
-       Tlc.set(startPin+1, 0);
-       Tlc.set(startPin+2, dimConst[startPin+2]);
+       Tlc.set(signalChannel[pntr]+0, dimConst[signalChannel[pntr]]);
+       Tlc.set(signalChannel[pntr]+1, 0);
+       Tlc.set(signalChannel[pntr]+2, dimConst[signalChannel[pntr]+2]);
        break;
     case 2: // zet ch1 uit en ch2, ch3 aan
 //       Serial.println(" Fahrt mit Vorsicht");
-       Tlc.set(startPin+0, 0);
-       Tlc.set(startPin+1, dimConst[startPin+1]);
-       Tlc.set(startPin+2, dimConst[startPin+2]);
+       Tlc.set(signalChannel[pntr]+0, 0);
+       Tlc.set(signalChannel[pntr]+1, dimConst[signalChannel[pntr]+1]);
+       Tlc.set(signalChannel[pntr]+2, dimConst[signalChannel[pntr]+2]);
        break;
     default:
        break;
        }
   Tlc.update();
 }
-//////////////////////
+
